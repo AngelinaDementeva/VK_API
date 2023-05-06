d1b44c338c5a8b3f9af65c578180c50711f44f67
DemaPage <47453447+AngelinaDementeva@users.noreply.github.com>
2023-05-07 00:53:38 +0300

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'port' => '5432',
+        'dbname' => 'mydatabase',
+        'user' => 'myuser',
+        'password' => 'mypassword',
+    ],
+];
